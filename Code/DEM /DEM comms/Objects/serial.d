a46c7ac2040ab91a2c863c99053d7782b51dfc89
Oludirad <oludirad@bitbucket.org>
2019-02-23 05:57:03 -0600

    DEM Communication Interface
    Serial Interface:
    - initialization
    - Serial transmit
    - USART ISR for serial receive
    
    Xbee Interface
    - Send function to send over serial interface
    - Receive function to receive over serial interface

---
+++ Code/DEM /DEM comms/Objects/serial.d
@@ -0,0 +1,11 @@
+.\objects\serial.o: serial.c
+.\objects\serial.o: serial.h
+.\objects\serial.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\serial.o: C:\Users\oludirad\Desktop\capstone dem\RTE\RTE_Components.h
+.\objects\serial.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\core_cm3.h
+.\objects\serial.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\serial.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\serial.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\serial.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\serial.o: C:\Keil_v5\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
+.\objects\serial.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
---
+++ Code/DEM /DEM comms/main.c
@@ -0,0 +1,33 @@
+#include"stdio.h"
+#include"stm32f10x.h"
+#include"serial.h"
+#include"xbee.h"
+#include"ledIO.h"
+
+int GLOBAL_BUFF_SIZE = 11;
+bool GLOBAL_DATA_RECEIVED =false;
+uint16_t GLOBAL_USART_Received_Data[30];
+int GLOBAL_CHAR_COUNTER = 0;
+
+
+int i = 0;
+
+uint16_t data[14] = {0x7e, 0x00, 0x0a, 0x01, 0x01, 0xff, 0xff, 0x00, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x0b};
+
+int main()
+{
+	xbee_init();
+	apbClockEnable();
+	configMode();
+	
+	while(1)
+	{
+//		xbee_send(0x00DC, 0x4567);
+//		for (i=0; i < 500000; i++)
+//		{}
+		if(xbee_recieve() == 0x4567)
+		{ GPIOC->ODR ^= GPIO_ODR_ODR8;}
+			
+	}
+}	
+
