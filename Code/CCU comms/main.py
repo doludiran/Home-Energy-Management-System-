cfb60d95cf80b77ef3f5b1767256202dbe2fc734
Oludirad <oludirad@bitbucket.org>
2019-03-02 00:53:55 -0600

    Xbee comms for CCU. send and receive methods.

--- Code/CCU comms/main.py
+++ Code/CCU comms/main.py
@@ -2,11 +2,18 @@ import xbee
 from time import sleep
 
 if __name__ == "__main__":
-    xbee = xbee.Xbee("/dev/tty.usbserial-14410")
-    data = [0x45, 0x32]
+    xbee = xbee.Xbee("/dev/ttyS0")
+    data = [0x58, 0x90]
     # A simple string message
-    sent = xbee.send(data, 0x00DE)
-    sleep(0.25)
     
-    while True:
+
+while True:
+#    xbee.send(data)
+#    sleep(0.25)
+#    rx =xbee.serial.read()
+#    print(rx)
+
+    xbee.recieve()
+#    print(message)
+    sleep(0.25)
 
--- Code/CCU comms/xbee.py
+++ Code/CCU comms/xbee.py
@@ -1,12 +1,12 @@
 import serial
 
 class Xbee():
-    def __init__(self, port = "/dev/tty.usbserial-14410", baudrate=9600):
+    def __init__(self, port = "/dev/ttyS0", baudrate=9600):
         self.serial = serial.Serial(port = port, baudrate=baudrate)
         to_send1 =[0x42]
         self.serial.write(serial.to_bytes(to_send1))
     
-    def send(self, data, address, frameid = 0x01, options=0x00):
+    def send(self, data, address=0x00DE, frameid = 0x01, options=0x00):
         if not data:
             return 0
         message = '7E 00 {:02X} 01 {:02X} {:02X} {:02X} {:02X}'.format(
@@ -18,17 +18,23 @@ class Xbee():
 
         frame.append(0xFF - sum(frame[3:]) & 0xFF)
 
-        print("Tx: " + self.format(frame))
+        #print("Tx: " + self.format(frame))
         return self.serial.write(frame)
 
     def recieve(self):
         message = []
-        i = 0
-        if (self.serial.read() == 0x7E)
-            for i < 11
-                message.append(self.serial.read())
-                i+=1
-        return message
+        i =0
+        if (self.serial.read() == bytes(b'\x7E')):
+            message.append('7e')
+            while (i < 10):
+                msg = self.serial.read().encode("hex")
+                message.append(msg)
+                i += 1
+            data = message[8] + message[9]
+            int_data =int(data, 16)
+        print(int_data)
+                
+        
 
 
 
